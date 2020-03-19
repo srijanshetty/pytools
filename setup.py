35c1f96dd5ede33b5eaa3760b13f72a63a561d53
Srijan R Shetty <srijan.shetty@gmail.com>
2020-03-19 13:32:53 +0530

    feat: package for common python tools

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='pytools',
+      version='0.1',
+      description='srijanshetty\'s common tools',
+      author='Srijan R Shetty',
+      author_email='srijan.shetty@gmail.com',
+      url='https://github.com/srijanshetty/pytools',
+      packages=['pytools'],
+      package_data={'pytools': ['*.txt']},
+      include_package_data=True)
